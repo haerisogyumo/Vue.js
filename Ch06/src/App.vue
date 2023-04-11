3e390ae005c5b3880b14f42df394d4913d4b0862
haerisogyumo <haerisogyumo@gmail.com>
2023-04-11 13:34:25 +0900

    20230411

---
+++ Ch06/src/App.vue
@@ -0,0 +1,37 @@
+import Component3 from './components/Component3.vue';
+
+<template>
+  <h3>Ch06.Vuex 실습</h3>
+  <p>Vuex message : {{ store.getters.getMsg }}</p>
+  <table border="1">
+    <tr>
+      <td>번호</td>
+      <td>내용</td>
+      <td>완료</td>
+    </tr>
+    <tr v-for="todo in todos">
+      <td>{{ todo.no }}</td>
+      <td>{{ todo.text }}</td>
+      <td>{{ todo.done }}</td>
+    </tr>
+  </table>
+  <Component1></Component1>
+  <Component2></Component2>
+  <Component3></Component3>
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import Component1 from "./components/Component1.vue";
+import Component2 from "./components/Component2.vue";
+import Component3 from "./components/Component3.vue";
+const store = useStore();
+const todos = store.getters.getTodos;
+</script>
+
+<style scoped>
+/**
+2023/04/11
+npm create vite@latest
+*/
+</style>
---
+++ Ch06/src/components/Component3.vue
@@ -0,0 +1,20 @@
+<template>
+  <h4>component3</h4>
+  <p>Vuex count : {{ store.getters.getCount }}</p>
+  <input type="number" v-model="count" />
+  <button @click="btnInit">초기화</button>
+</template>
+<script setup>
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const count = ref(0);
+
+const btnInit = function () {
+  const payload = Number(count.value);
+  // store action 호출
+  store.dispatch("initCount", payload);
+};
+</script>
+<style scoped></style>
